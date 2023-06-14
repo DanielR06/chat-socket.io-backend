82ad91b89000d6391d5ca0337a6871b37e7787f7
Daniel Rodriguez <kungary04@gmail.com>
2023-06-14 03:37:47 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express  from "express";
+import morgan from "morgan";
+import { Server as SocketServer} from "socket.io";
+import http from 'http'
+import cors from "cors";
+
+import { PORT } from "./config.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+    cors: {
+        origin:'http://localhost:5173'
+    }
+});
+
+app.use(cors);
+app.use(morgan('dev'));
+
+io.on('connection', (socket) =>{
+    console.log(socket.id);
+    console.log('A user connected');
+    
+    socket.on('message', (message) => {
+        socket.broadcast.emit('message', {
+            body:message,
+            from:socket.id
+        });
+    });
+});
+
+server.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
