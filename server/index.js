5299fa915dba687314d70993f07d2daa293110b7
Daniel Rodriguez <kungary04@gmail.com>
2023-06-14 04:00:35 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
-        origin:'http://localhost:5173'
+        origin:'https://chat-web-socket-io.netlify.app'
     }
 });
 
