4a8b3c195633f80f23ecf29e2726380277ce294c
DanielR06 <kungary04@gmail.com>
2023-07-15 01:53:23 -0500

    Setup Router

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
-        origin: ORIGIN
+        origin: "*"
     }
 });
 
@@ -20,12 +20,21 @@ app.use(morgan('dev'));
 io.on('connection', (socket) =>{
     console.log(socket.id);
     console.log('A user connected');
-    
+    const count = io.engine.clientsCount;
+    console.log(count)
     socket.on('message', (message) => {
         socket.broadcast.emit('message', {
             body:message,
             from:socket.id
         });
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+
+        console.log(io.engine.clientsCount); // Eliminar usuario desconectado
+
+        // Actualizar la lista de usuarios conectados en el cliente
+        //io.emit('users', Array.from(connectedUsers.keys()));
     });
 });
 
